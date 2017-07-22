fba2e4adb195d99326b7768cd640134e5d2c6ad2
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2017-07-21 20:43:49 -0400

    Catch-up.

--- tests/catalog_tool_tests.py
+++ tests/catalog_tool_tests.py
@@ -128,8 +128,8 @@ class TestFullInitializationPrepping(unittest.TestCase):
         n_tasks = len(task_folders)
         n_expected = len(set([entry['resource'] for entry in glossary]))
 
-        assert all([os.path.isfile("./temp/catalog/{0}/datapackage.json".format(cf)) for cf in os.listdir(
-            "./temp/catalog")])
+        assert all([os.path.isfile("./temp/catalog/{0}/datapackage.json".format(cf)) for cf in
+                    os.listdir("./temp/catalog")])
 
         assert n_catalog == n_expected
         assert n_tasks == n_expected
