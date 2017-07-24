d9670108b8925e0bd00a8238e1653f036adae2ad
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2017-07-23 20:40:26 -0400

    Tweak test.

--- tests/workflow_dag_tests.py
+++ tests/workflow_dag_tests.py
@@ -2,20 +2,21 @@
 Unit tests for the DAG generated as a part of the portal localization workflow. See also the IO tests.
 """
 
-from ast import literal_eval
-import json
 import os
-from pathlib import Path
 import unittest
 import shutil
 
 import sys; sys.path.insert(0, './../')
 # noinspection PyUnresolvedReferences
 from urban_physiology_toolkit.workflow_utils import (init_catalog, update_dag)
-from airscooter.orchestration import configure
+# from airscooter.orchestration import configure
 
 
-class TestSimpleResourceDAG(unittest.TestCase):
+class TestSimpleResourceDAGSmokeTest(unittest.TestCase):
+    """
+    Quick smoke test. Makes sure that writing the DAG succeeds, but doesn't evaluate it.
+    """
+
     def setUp(self):
         os.mkdir("temp")
         os.mkdir("temp/.airflow/")
@@ -25,9 +26,7 @@ class TestSimpleResourceDAG(unittest.TestCase):
         init_catalog("./data/blob_resource_glossary.json", "temp")
         update_dag(root="./temp")
 
-        # TODO
-        import pdb; pdb.set_trace()
-        pass
+        assert set(os.listdir("./temp/.airflow")) == {'airscooter.yml', 'datablocks_dag.py'}
 
     def tearDown(self):
         shutil.rmtree("temp")
