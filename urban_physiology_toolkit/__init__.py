7fac51308e84d74089ea00e9e3abb1cff368c550
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2017-08-23 21:00:33 -0400

    Tweak init.

--- urban_physiology_toolkit/__init__.py
+++ urban_physiology_toolkit/__init__.py
@@ -1,6 +1,1 @@
-# from .ckan_glossarizer import (write_resource_representation, write_glossary)
-# from .socrata_glossarizer import (get_resource_representation, write_resource_representation, get_glossary,
-#                                   write_glossary)
-
-# Don't import pager because it inits Ghostdriver+PhantomJS which massively slows down typing due to a bug.
-# from .pager import *
+from .glossarizers import *
